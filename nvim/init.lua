d440c7140be043352a48a1a90c0a40b1cf34118e
Ozmar Mendoza <101409227+ozzy1136@users.noreply.github.com>
2024-09-09 12:47:56 -0500

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,5 @@
-
+vim.opt.number = true
+vim.opt.ignorecase = true
+vim.keymap.set( "i", "jk", "<esc>")
+vim.g.mapleader = "'"
+vim.keymap.set("n", "<Leader>zz", ":let &scrolloff=999-&scrolloff<cr>")
