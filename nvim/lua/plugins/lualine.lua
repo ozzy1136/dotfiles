cc221eee260862e3d6866a06263463f31ba263c4
Ozmar Mendoza <101409227+ozzy1136@users.noreply.github.com>
2024-09-19 17:17:36 -0500

    Update lualine.lua

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ return {
         "nvim-lualine/lualine.nvim",
         dependencies = {"nvim-tree/nvim-web-devicons"},
         opts = {
-            options = {theme = "material"},
+            options = {theme = "tokyonight"},
             sections = {
                 lualine_c = {"harpoon2"},
                 lualine_x = {
