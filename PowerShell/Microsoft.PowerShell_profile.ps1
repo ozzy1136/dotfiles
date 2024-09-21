b92335abd12ce5134e54386a5a8126225c304c6d
Ozmar Mendoza <101409227+ozzy1136@users.noreply.github.com>
2024-09-20 23:59:12 -0500

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -22,4 +22,6 @@ function komorebi-start {
   komorebic start --whkd
 }
 
-$Env:KOMOREBI_CONFIG_HOME = 'C:\Users\{User}\.config\komorebi'
+$Env:KOMOREBI_CONFIG_HOME = "C:\Users\{User}\.config\komorebi"
+
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/tokyo.omp.json" | Invoke-Expression
