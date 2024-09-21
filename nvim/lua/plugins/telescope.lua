121d68b37a2c12e498bd2a2dd634771c3584365c
Ozmar Mendoza <101409227+ozzy1136@users.noreply.github.com>
2024-09-21 01:53:15 -0500

    Update telescope.lua

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -10,7 +10,32 @@ return {
             "BurntSushi/ripgrep", "sharkdp/fd",
         },
         config = function()
-            require("telescope").setup({
+            local telescope = require("telescope")
+            local telescopeConfig = require("telescope.config")
+
+            -- Clone the default Telescope configuration
+            local vimgrep_arguments = {
+                unpack(telescopeConfig.values.vimgrep_arguments),
+            }
+
+            table.insert(vimgrep_arguments, "--hidden")
+            table.insert(vimgrep_arguments, "--glob")
+            table.insert(vimgrep_arguments, "!**/.git/*")
+
+            telescope.setup({
+                defaults = {
+                    -- `hidden = true` is not supported in text grep commands.
+                    vimgrep_arguments = vimgrep_arguments,
+                },
+                pickers = {
+                    find_files = {
+                        find_command = {
+                            -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d
+                            "rg", "--files", "--hidden", "--glob", "!**/.git/*",
+                            "--unrestricted",
+                        },
+                    },
+                },
                 extensions = {
                     ["ui-select"] = {require("telescope.themes").get_dropdown()},
                 },
