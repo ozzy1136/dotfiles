d2aa2ed669f09d68fe341207ca1e60b166a859a7
Ozmar Mendoza <101409227+ozzy1136@users.noreply.github.com>
2024-09-21 17:03:50 -0500

    Create flexoki.lua

---
+++ nvim/lua/plugins/flexoki.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "kepano/flexoki-neovim",
+        priority = 1000, -- Make sure to load this before all the other start plugins.
+        name = "flexoki",
+        init = function()
+            vim.cmd("colorscheme flexoki-dark")
+        end,
+    },
+}
