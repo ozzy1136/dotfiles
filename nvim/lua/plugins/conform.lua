49b856bdc8cf0cba78ef488e2452eac1ec8945a7
Ozmar Mendoza <101409227+ozzy1136@users.noreply.github.com>
2026-02-16 19:22:11 -0600

    Update conform.lua

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,33 +1,31 @@
 return {
-    {
-        "stevearc/conform.nvim",
-        event = {"BufWritePre"},
-        cmd = {"ConformInfo"},
-        keys = {
-            {
-                "<leader>f",
-                function()
-                    require("conform").format {
-                        async = true,
-                        lsp_format = "fallback",
-                    }
-                end,
-                mode = "",
-                desc = "[F]ormat buffer",
-            },
-        },
-        config = function()
-            local conform = require("conform")
-            conform.setup({
-                notify_on_error = false,
-                formatters_by_ft = {
-                    lua = {"lua-format"},
-                    javascript = {"eslint_d", "prettierd"},
-                    javascriptreact = {"eslint_d", "prettierd"},
-                    css = {"prettierd"},
-                },
-                format_on_save = {timeout_ms = 500, lsp_format = "fallback"},
-            })
-        end,
-    },
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		keys = {
+			{
+				"<leader>f",
+				function()
+					require("conform").format {
+						async = true,
+						lsp_format = "fallback",
+					}
+				end,
+				mode = "",
+				desc = "[F]ormat buffer",
+			},
+		},
+		config = function()
+			require("conform").setup({
+				notify_on_error = false,
+				formatters_by_ft = {
+					lua = { "lua-format" },
+					javascript = { "eslint_d", "prettierd" },
+					javascriptreact = { "eslint_d", "prettierd" },
+					css = { "prettierd" },
+				},
+				format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
+			})
+		end,
+	},
 }
