de5b0fd7f8fe18c438763c5331ead2e873bc0294
Ozmar Mendoza <101409227+ozzy1136@users.noreply.github.com>
2026-02-16 19:21:30 -0600

    Update luasnip.lua

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,23 +1,14 @@
 return {
-    {
-        "L3MON4D3/LuaSnip",
-        version = "v2.*",
-        build = function()
-            -- Build Step is needed for regex support in snippets.
-            -- This step is not supported in many windows environments.
-            -- Remove the below condition to re-enable on windows.
-            -- if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-            --     return
-            -- end
-            -- return 'make install_jsregexp'
-            return "mingw32-make install_jsregexp"
-
-        end,
-        dependencies = {"rafamadriz/friendly-snippets"},
-    }, {
-        "rafamadriz/friendly-snippets",
-        config = function()
-            require("luasnip.loaders.from_vscode").lazy_load()
-        end,
-    },
+	{
+		"L3MON4D3/LuaSnip",
+		version = "v2.*",
+		build = "make install_jsregexp",
+		dependencies = { "rafamadriz/friendly-snippets" },
+	},
+	{
+		"rafamadriz/friendly-snippets",
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
 }
