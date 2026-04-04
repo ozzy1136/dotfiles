80e38128b624d5b3231e7ff665c03ed359ee08fb
Ozmar Mendoza <amfc11@gmail.com>
2026-04-03 21:11:37 -0500

    chezmoi init

---
+++ private_dot_config/nvim/lua/plugins/luasnip.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"L3MON4D3/LuaSnip",
+		version = "v2.*",
+		build = "make install_jsregexp",
+		dependencies = { "rafamadriz/friendly-snippets" },
+	},
+	{
+		"rafamadriz/friendly-snippets",
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
+}
---
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			local treesitter = require('nvim-treesitter')
+			treesitter.setup {}
+			treesitter.install { "css", "html", "javascript", "jsdoc", "json", "lua", "regex", "sql", "svelte", "typescript", "yaml" }
+			vim.api.nvim_create_autocmd('FileType', {
+				pattern = { '<filetype>' },
+				callback = function()
+					vim.treesitter.start()
+					vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+					vim.wo[0][0].foldmethod = 'expr'
+					vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+				end,
+			})
+		end,
+	},
+}
